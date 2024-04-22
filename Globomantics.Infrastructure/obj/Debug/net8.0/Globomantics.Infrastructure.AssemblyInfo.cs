238cb7a98f3d06ad682557c33f40bb30afdffd78
Maciej Broda <maciejbroda01@gmail.com>
2024-04-22 18:31:39 +0200

    Creating a Repository

--- Globomantics.Infrastructure/obj/Debug/net8.0/Globomantics.Infrastructure.AssemblyInfo.cs
+++ Globomantics.Infrastructure/obj/Debug/net8.0/Globomantics.Infrastructure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Globomantics.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0d3912836c56b0eaf3dcc22d8b66a8aae8af1da7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d67ccf9cbb47c0b2b7151b0fea6fddb213648fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Globomantics.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Globomantics.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Globomantics.Windows/obj/Debug/net8.0-windows/Globomantics.Windows.AssemblyInfo.cs
+++ Globomantics.Windows/obj/Debug/net8.0-windows/Globomantics.Windows.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Globomantics.Windows")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c76291159679e769a57cfe915c20e71c5a92a824")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d67ccf9cbb47c0b2b7151b0fea6fddb213648fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("Globomantics.Windows")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Globomantics.Windows")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
